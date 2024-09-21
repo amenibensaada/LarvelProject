192849a9724bf2c96eb4be84603d74f302085bf9
kaliltebessi <124347501+kaliltebessi@users.noreply.github.com>
2024-09-21 18:29:27 +0100

    Update login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-            <h1> front Login</h1>
+           
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
